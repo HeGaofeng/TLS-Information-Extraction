9b89fd82afc0d79cda4fe3df37c58d486b0604d3
HeGaofeng <gaofeng.he@outlook.com>
2020-04-26 09:59:21 +0800

    Add files via upload

--- basic-tshark-joy-bro-tls.sh
+++ basic-tshark-joy-bro-tls.sh
@@ -5,19 +5,31 @@ if [ ! -d $1-tls-flows ]; then
 	mkdir $1-tls-flows
 fi
 
+cp $1 $1-tls-flows
+mv $2 $1-tls-flows
+
+cd $1-tls-flows
+
 for stream in `tshark -r $1 -Y tls -T fields -e tcp.stream | sort -n | uniq`
 do
     echo "Extract meta informaion from TLS flow $stream"
     tshark -r $1 -F pcap -w $1-tls-$stream.pcap -2 -R "tcp.stream==$stream"
     tshark -r $1-tls-$stream.pcap -T fields -c 1 -e ip.dst >  $1-tls-$stream.ip-names.txt
+   	#find out all server_names(CNAMEs) for ip.dst
    	grep -f $1-tls-$stream.ip-names.txt $2 | awk '{print $2}'| awk 'BEGIN{ FS=","} {for( i=1; i<=NF; i++) print $i}' >>  $1-tls-$stream.ip-names.txt
+    #use jor to obtain the first 200 packets' sizes and time intervals
     joy output=$1-tls-$stream.json bidir=1 num_pkts=200 ppi=0 zeros=1 $1-tls-$stream.pcap
+   	#use bro to obtain ssl and x.509 information
     bro -C -r $1-tls-$stream.pcap
-    mv ssl.log $1-tls-$stream.ssl.log
-    mv x509.log $1-tls-$stream.x509.log
-    mv $1-tls-$stream.* $1-tls-flows
+    	
+   	if [ -f "ssl.log" ]; then 
+   		mv ssl.log $1-tls-$stream.ssl.log
+   	fi
+    if [ -f "x509.log" ]; then
+    	mv x509.log $1-tls-$stream.x509.log
+    fi
 done
-rm conn.log
-rm files.log
-rm packet_filter.log
+
+#rm files.log
+#rm packet_filter.log
 
--- tls-flows.sh
+++ tls-flows.sh
@@ -13,13 +13,34 @@ if [ ! -d "$1" ]; then
 	exit
 fi
 
+tmp_fifofile="/tmp/$$.fifo" 
+mkfifo $tmp_fifofile
+exec 1000<>$tmp_fifofile
+rm -fr $tmp_fifofile
+
+trap "exec 1000>&-;exec 1000<&-;exit 0" 2 #CTRL-C
+
+thread_num=2
+
+for ((i=0;i<${thread_num};i++));do # thread_num tokens
+	echo
+done >&1000
+
 cd "$1"
 
 for file in `find . -maxdepth 1 -type f -name '*.pcap' -o -name '*.cap' -o -name '*.pcapng'`
 do
-	echo "analyze $file"
-	tshark -r $file -Y "dns.flags.response == 1" -T fields -e dns.qry.name -e dns.resp.name -e dns.a > $file-allDNSs.txt
-	basic-tshark-joy-bro-tls.sh $file $file-allDNSs.txt
+	read -u1000
+	{
+		echo "analyze $file"
+		tshark -r $file -Y "dns.flags.response == 1" -T fields -e dns.qry.name -e dns.resp.name -e dns.a > $file-allDNSs.txt
+		basic-tshark-joy-bro-tls.sh $file $file-allDNSs.txt
+		echo >&1000 # release token
+	}&	#backgroud process 
 done
+
+wait
+exec 1000>&-
+exec 1000<&-
 
 echo "TLS flows extraction finished."
