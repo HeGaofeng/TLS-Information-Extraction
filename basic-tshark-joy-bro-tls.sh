996b6dc0d4254d9aa38281ed40daef5ae3b7ed5a
HeGaofeng <gaofeng.he@outlook.com>
2020-05-09 22:19:56 +0800

    Add files via upload

--- basic-tshark-joy-bro-tls.sh
+++ basic-tshark-joy-bro-tls.sh
@@ -7,14 +7,10 @@ fi
 
 cp $1 $1-tls-flows
 mv $2 $1-tls-flows
-#if [ -f "dns.log" ]; then 
-#	mv dns.log $1-tls-flows
-#fi
-
-#rm *.log
 
 cd $1-tls-flows
 
+# generate bro logs for the original pcap file
 bro -C -r $1
 
 for stream in `tshark -r $1 -Y tls -T fields -e tcp.stream | sort -n | uniq`
@@ -23,26 +19,37 @@ do
     if [ ! -d $1-tls-$stream ]; then
 		mkdir $1-tls-$stream
 	fi
+	# TLS flows are saved in seperated folders
 	cd $1-tls-$stream
+	# extract one TLS flow from the original pcap file
     tshark -r ../$1 -F pcap -w $1-tls-$stream.pcap -2 -R "tcp.stream==$stream"
-    #tshark -r $1-tls-$stream.pcap -T fields -c 1 -e ip.dst >  $1-tls-$stream.ip-names.txt
-    tshark -r $1-tls-$stream.pcap -T fields -c 1 -e ip.dst > ip-names.txt
+    
+    # extract the dst IP
+    tshark -r $1-tls-$stream.pcap -T fields -c 1 -e ip.dst >  $1-tls-$stream.ip-names.txt
+    #tshark -r $1-tls-$stream.pcap -T fields -c 1 -e ip.dst > ip-names.txt
     
    	#find out all server_names(CNAMEs) for ip.dst
-   	grep -f ip-names.txt ../$2 | awk '{print $2}'| awk 'BEGIN{ FS=","} {for( i=1; i<=NF; i++) print $i}' >>  ip-names.txt
+   	grep -f $1-tls-$stream.ip-names.txt ../$2 | awk '{print $2}'| awk 'BEGIN{ FS=","} {for( i=1; i<=NF; i++) print $i}' >> $1-tls-$stream.ip-names.txt
+   	#grep -f ip-names.txt ../$2 | awk '{print $2}'| awk 'BEGIN{ FS=","} {for( i=1; i<=NF; i++) print $i}' >>  ip-names.txt
+    
     #use joy to obtain the first 200 packets' sizes and time intervals
-    #joy output=$1-tls-$stream.json bidir=1 num_pkts=200 ppi=0 zeros=1 $1-tls-$stream.pcap
-    joy output=joy.json bidir=1 num_pkts=200 ppi=0 zeros=1 $1-tls-$stream.pcap
+    joy output=$1-tls-$stream.json bidir=1 num_pkts=200 ppi=0 zeros=0 $1-tls-$stream.pcap
+    #joy output=joy.json bidir=1 num_pkts=200 ppi=0 zeros=1 $1-tls-$stream.pcap
+   	
    	#use bro to obtain ssl and x.509 information
     bro -C -r $1-tls-$stream.pcap
-    cd ..
+    #cd ..
     	
-   	#if [ -f "ssl.log" ]; then 
-   	#	mv ssl.log $1-tls-$stream.ssl.log
-   	#fi
-    #if [ -f "x509.log" ]; then
-    #	mv x509.log $1-tls-$stream.x509.log
-    #fi
+    # rename ssl.log and x509.log
+   	if [ -f "ssl.log" ]; then 
+   		mv ssl.log $1-tls-$stream.ssl.log
+   	fi
+    if [ -f "x509.log" ]; then
+    	mv x509.log $1-tls-$stream.x509.log
+    fi
+    
+    # return to the parent directory
+    cd ..
 done
 
 #rm files.log
